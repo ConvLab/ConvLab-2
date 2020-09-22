231abcb9e3e8889271900dc088cb1aa49e65e6a9
function2 <function2@qq.com>
2020-09-22 18:14:32 +0800

    sync with thu-coai/ConvLab-2 54c57c59ae531a16fc69e6cf82e5844e87780f78

--- eval/eval_file.py
+++ eval/eval_file.py
@@ -2,27 +2,17 @@
     evaluate output file
 """
 
-from convlab2.dst.dstc9.utils import prepare_data, eval_states
+from convlab2.dst.dstc9.eval_file import extract_gt, dump_example, prepare_data, evaluate
 
 if __name__ == '__main__':
-    import os
-    import json
     from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument('subtask', type=str, choices=['multiwoz', 'crosswoz'])
+    parser.add_argument('split', type=str, choices=['train', 'val', 'test', 'human_val'])
     args = parser.parse_args()
-
-    gt = {
-        dialog_id: [state for _, _, state in turns]
-        for dialog_id, turns in prepare_data(args.subtask).items()
-    }
-
-    results = {}
-    for i in range(1, 6):
-        filename = f'submission{i}.json'
-        if not os.path.exists(filename):
-            continue
-        pred = json.load(open(filename))
-        results[filename] = eval_states(gt, pred)
-
-    json.dump(results, open('results.json', 'w'), indent=4, ensure_ascii=False)
+    subtask = args.subtask
+    split = args.split
+    dump_example(subtask, split)
+    test_data = prepare_data(subtask, args.split)
+    gt = extract_gt(test_data)
+    evaluate('example', subtask, gt)
--- eval/eval_model.py
+++ eval/eval_model.py
@@ -3,10 +3,18 @@
 """
 
 from convlab2.dst.dstc9.eval_model import evaluate
+from convlab2.dst.dstc9.utils import prepare_data
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument('subtask', type=str, choices=['multiwoz', 'crosswoz'])
+    parser.add_argument('split', type=str, choices=['train', 'val', 'test', 'human_val'])
     args = parser.parse_args()
-    evaluate('example', args.subtask)
+    subtask = args.subtask
+    test_data = prepare_data(subtask, args.split)
+    gt = {
+        dialog_id: [state for _, _, state in turns]
+        for dialog_id, turns in test_data.items()
+    }
+    evaluate('example', subtask, test_data, gt)
