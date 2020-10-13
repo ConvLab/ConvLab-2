0f06505ac9a5c80dabd750e9aca5e9eb878605f4
function2 <function2@qq.com>
2020-10-13 12:06:53 +0800

    udpate eval

--- eval/eval_model.py
+++ eval/eval_model.py
@@ -10,9 +10,9 @@ from convlab2.dst.dstc9.utils import prepare_data, extract_gt
 
 def eval_team(team):
     for subtask in ['multiwoz', 'crosswoz']:
-        test_data = prepare_data(subtask, 'dstc9-250')
+        test_data = prepare_data(subtask, 'dstc9')
         gt = extract_gt(test_data)
-        for i in range(2, 6):
+        for i in range(1, 6):
             model_dir = os.path.join(team, f'{subtask}-dst', f'submission{i}')
             if not os.path.exists(model_dir):
                 continue
@@ -20,6 +20,7 @@ def eval_team(team):
             evaluate(model_dir, subtask, test_data, gt)
 
 if __name__ == '__main__':
-    eval_team('H1kkkkkkkk')
-    # eval_team('adamlin120')
-    # eval_team('myhexinfishzhou')
+    for team in os.listdir('.'):
+        if not os.path.isdir(team):
+            continue
+        eval_team(team)
